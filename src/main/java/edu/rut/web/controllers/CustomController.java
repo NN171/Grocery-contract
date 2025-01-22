866b43b66733c7c64bdf36e89e323aec7d5981ce
nn17 <yakor356@gmail.com>
2025-01-22 08:44:25 +0300

    add doc

--- src/main/java/edu/rut/web/controllers/CustomController.java
+++ src/main/java/edu/rut/web/controllers/CustomController.java
@@ -9,9 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customs")
 public interface CustomController {
 
+	/**
+	 * Вывод высокооцененных товаров
+	 *
+	 * @return - Страница с товарами
+	 */
 	@GetMapping("/highrates")
 	String getHighRated(Model model);
 
+	/**
+	 * Вывод популярных товаров пользователя
+	 *
+	 * @param userDetails - данные пользователя
+	 * @return - Страница популярных товаров
+	 */
 	@GetMapping("/frequents")
 	String getFrequents(Model model,
 						@AuthenticationPrincipal UserDetails userDetails);
